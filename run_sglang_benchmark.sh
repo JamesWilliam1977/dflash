7e27327ad70a0ef5ce692f5f8767b5132752d0c1
Jian Chen <jianchen0311@gmail.com>
2026-03-14 19:07:16 -0700

    Enable longer context length for SGLang
    Allow longer context length in SGLang benchmark.

--- run_sglang_benchmark.sh
+++ run_sglang_benchmark.sh
@@ -2,6 +2,8 @@ mkdir -p logs
 
 datasets=(gsm8k math500 humaneval mbpp mt-bench alpaca)
 
+export SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN=1
+
 for i in "${!datasets[@]}"; do
   ds="${datasets[$i]}"
 
